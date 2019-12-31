78b07bdba61f2b52f8e30f5d15f614a0cb427bcd
Rasmus Ansin <rasmus.ansin@gmail.com>
2019-12-31 13:33:18 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="tablify",
+    version="0.1",
+    author="Rasmus Ansin",
+    author_email="rasmus.ansin@gmail.com",
+    url="https://github.com/Ran4/tablify",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
